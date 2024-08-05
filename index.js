2094cdfb52a6d58f615747f47d5cbaa963631811
Matt Braddock <matt.braddock@gmail.com>
2024-08-05 16:53:03 -0400

    initial setup

---
+++ index.js
@@ -0,0 +1,35 @@
+import { writeFileSync } from 'node:fs';
+import * as fsScandir from '@nodelib/fs.scandir';
+
+const ignore = [
+    '.gitignore',
+    'files.json',
+    'index.html',
+    'index.js',
+    'node_modules',
+    'package-lock.json',
+    'package.json'
+];
+
+let directories = ['.'];
+let entries = [];
+let data = {};
+
+do {
+    const directory = directories.shift();
+    entries = fsScandir.scandirSync(directory, { followSymbolicLinks: true, stats: true });
+    if (directory === '.') entries = entries.filter(entry => ignore.includes(entry.name) === false);
+    directories = [...directories, ...entries.filter(entry => entry.dirent.isDirectory()).map(entry => entry.path)];
+    data = {
+        ...data,
+        [directory]: entries.map(entry => ({
+            name: entry.name,
+            path: entry.path,
+            dir: entry.dirent.isDirectory(),
+            size: entry.stats.size,
+            date: new Date(entry.stats.mtimeMs).toUTCString()
+        }))
+    };
+} while (directories.length > 0);
+
+writeFileSync('files.json', JSON.stringify(data));
